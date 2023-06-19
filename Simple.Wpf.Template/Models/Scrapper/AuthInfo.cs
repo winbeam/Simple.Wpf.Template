db774a6e08d10667b481a9b7f49b1156ec09f9a6
winbeam <winbeom@gmail.com>
2023-06-19 23:41:00 +0900

    authinfo 8 digits

--- Simple.Wpf.Template/Models/Scrapper/AuthInfo.cs
+++ Simple.Wpf.Template/Models/Scrapper/AuthInfo.cs
@@ -20,16 +20,28 @@ public class AuthInfo
     string _phone = "12341234";
     public string Phone
     {
-        get { return _phone; }
-        set 
+        get => _phone;
+        set
         {
+            if (value.Length > 8)
+                value = value.Substring(0, 8);
             _phone = Regex.Replace(value, @"[^0-9]", "");
             if (_phone.StartsWith("010"))
                 _phone = _phone[3..];
         }
     }
 
-    public string Birth { get; set; } = "19900101";
+    string _birth = "19900101";
+    public string Birth
+    {
+        get => _birth;
+        set
+        {
+            if (value.Length > 8)
+                value = value.Substring(0, 8);
+            _birth = Regex.Replace(value, @"[^0-9]", "");
+        }
+    }
     public MobileCarrier MobileCarrier { get; set; }  //  for PASS
 
 }
